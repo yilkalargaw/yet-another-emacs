256a8f350f132ec6fa76f3fbfbb35e8baf4c3ffe
yilkalargaw <yilkalargawworkneh@gmail.com>
2019-05-02 00:32:05 +0300

    refactored the packagearchives part

--- init.el
+++ init.el
@@ -6,15 +6,12 @@
 
 (require 'package)
 
-;; If you want  to use the latest version
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-;; If you want to use the last tagged version
-(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 
-;;(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-
-(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq package-archives '( ;; ("marmalade" . "http://marmalade-repo.org/packages/")
+			 ("org"       . "http://orgmode.org/elpa/")
+                         ("gnu"       . "http://elpa.gnu.org/packages/")
+			 ("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ("melpa"     . "http://melpa.org/packages/")))
 
 
 ;increase garbage collection threshold for startup only
@@ -22,6 +19,7 @@
 (add-hook 'after-init-hook #'(lambda ()
                                ;; restore after startup
                                (setq gc-cons-threshold 800000)))
+
 
 ;; directories for custom themes and elsip-scripts
 (add-to-list 'load-path "~/.emacs.d/lisp")
