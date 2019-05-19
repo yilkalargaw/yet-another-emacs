cd7176b5542bd6f0c9b4af300e2d39baed9e15c0
yilkalargaw <yilkalargawworkneh@gmail.com>
2019-05-19 17:16:44 +0300

    lexically setted file-name-handler-alist to nil to speedup init time

--- init.el
+++ init.el
@@ -3,33 +3,33 @@
 ;;; Code:
 
 ;;(package-initialize)
-
-(require 'package)
-
-
-(setq package-archives '( ;; ("marmalade" . "http://marmalade-repo.org/packages/")
-			 ("org"       . "http://orgmode.org/elpa/")
-                         ("gnu"       . "http://elpa.gnu.org/packages/")
-			 ("melpa-stable" . "https://stable.melpa.org/packages/")
-                         ("melpa"     . "http://melpa.org/packages/")))
+(let ((file-name-handler-alist nil))
+  (require 'package)
 
 
-;increase garbage collection threshold for startup only
-(setq gc-cons-threshold 64000000)
-(add-hook 'after-init-hook #'(lambda ()
-                               ;; restore after startup
-                               (setq gc-cons-threshold 800000)))
-
-;;Set custom settings to load in own file
-(setq custom-file (make-temp-file "emacs-custom"))
+  (setq package-archives '( ;; ("marmalade" . "http://marmalade-repo.org/packages/")
+			   ("org"       . "http://orgmode.org/elpa/")
+			   ("gnu"       . "http://elpa.gnu.org/packages/")
+			   ("melpa-stable" . "https://stable.melpa.org/packages/")
+			   ("melpa"     . "http://melpa.org/packages/")))
 
 
-;; directories for custom themes and elsip-scripts
-(add-to-list 'load-path "~/.emacs.d/lisp")
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+					;increase garbage collection threshold for startup only
+  (setq gc-cons-threshold 64000000)
+  (add-hook 'after-init-hook #'(lambda ()
+				 ;; restore after startup
+				 (setq gc-cons-threshold 800000)))
 
-;;main setupfile loaded using org-bable
-(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+  ;;Set custom settings to load in own file
+  (setq custom-file (make-temp-file "emacs-custom"))
 
-(provide 'init)
+
+  ;; directories for custom themes and elsip-scripts
+  (add-to-list 'load-path "~/.emacs.d/lisp")
+  (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+
+  ;;main setupfile loaded using org-bable
+  (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+(provide 'init))
 ;;; init.el ends here
