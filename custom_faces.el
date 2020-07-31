3e1a0a2ec6dc5c0bd1909f12433bbdabd2a50aa9
lispy-dobby <yilkalargawworkneh@gmail.com>
2020-07-31 14:27:38 +0000

    updated custom_faces.el

--- custom_faces.el
+++ custom_faces.el
@@ -24,17 +24,40 @@
 
   (custom-set-faces
    ;; `(org-agenda-structure ((t (:inherit default ,@sans-font :height 2.0 :underline nil))))
-   `(org-level-8 ((t (:inherit 'outline-8  :weight bold :height 1.0))))
-   `(org-level-7 ((t (:inherit 'outline-7  :weight bold :height 1.0))))
-   `(org-level-6 ((t (:inherit 'outline-6  :weight bold :height 1.0))))
-   `(org-level-5 ((t (:inherit 'outline-5  :weight bold :height 1.0))))
-   `(org-level-4 ((t (:inherit 'outline-4  :weight bold :height 1.0))))
-   `(org-level-3 ((t (:inherit 'outline-3  :weight bold :height 1.0))))
-   `(org-level-2 ((t (:inherit 'outline-2  :weight bold :height 1.0))))
-   `(org-level-1 ((t (:inherit 'outline-1  :weight bold :height 1.0))))
-   `(org-document-title ((t (:family "sans" :inherit org-level-1 :height 1.5 :underline nil :box ))))
+   `(org-level-8 ((t (:inherit 'outline-8
+			       ;; :weight bold
+			       :height 1.0))))
+   `(org-level-7 ((t (:inherit 'outline-7
+			       ;; :weight bold
+			       :height 1.0))))
+   `(org-level-6 ((t (:inherit 'outline-6
+			       ;; :weight bold
+			       :height 1.0))))
+   `(org-level-5 ((t (:inherit 'outline-5
+			       ;; :weight bold
+			       :height 1.0))))
+   `(org-level-4 ((t (:inherit 'outline-4
+			       ;; :weight bold
+			       :height 1.0))))
+   `(org-level-3 ((t (:inherit 'outline-3
+			       ;; :weight bold
+			       :height 1.0))))
+   `(org-level-2 ((t (:inherit 'outline-2
+			       ;; :weight bold
+			       :height 1.0))))
+   `(org-level-1 ((t (:inherit 'outline-1
+			       ;; :weight bold
+			       :height 1.0))))
 
-   `(org-block ((t (:foreground ,base-font-color :background ,background-color :box nil))))
+   `(org-document-title ((t (:family "sans"
+				     :inherit org-level-1
+				     :height 1.5
+				     :underline nil
+				     :box ))))
+
+   `(org-block ((t (:foreground ,base-font-color
+				:background nil :box nil))))
+
    ;; `(org-block-begin-line ((t ,org-highlights)))
    ;; `(org-block-end-line ((t ,org-highlights)))
    ;; `(org-block ((t (:background ,(face-background 'highlight)))))
@@ -50,28 +73,28 @@
 
    `(org-headline-done ((t (:strike-through t))))
    `(org-done ((t (:strike-through t :underline t :overline t))))
-   `(org-todo ((t (:foreground ,(face-foreground 'default) :background ,(face-background 'default)
+   `(org-todo ((t (:foreground ,(face-foreground 'default) :background nil
 			       :underline t :overline t))))
 
    ;; `(symbol-overlay-default-face ((t (:background ,(color-lighten-name (face-background 'default) 10)
    ;; 						    :foreground ,(color-darken-name (face-foreground 'default) 10)))))
 
-   `(symbol-overlay-default-face ((t (:background ,(face-foreground 'font-lock-comment-face)
-   						    :foreground ,(face-background 'font-lock-comment-face)))))
+   ;; `(symbol-overlay-default-face ((t (:background ,(face-foreground 'font-lock-comment-face)
+   ;; 						  :foreground ,(face-background 'font-lock-comment-face)))))
 
    `(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0))))
    ;; `(mode-line ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'default) :box ,(face-foreground 'link)))))
    ;; `(mode-line-inactive ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'default) :box nil))))
    ;; '(mode-line ((t (:background "#002b36"  :foreground "white" :box "dark cyan"))))
    ;; '(mode-line-inactive ((t (:background "#002b36" :foreground "white" :box nil))))
-   `(font-lock-comment-face ((t (:background ,(face-background 'default) :slant italic))))
+   `(font-lock-comment-face ((t (:background nil :slant italic))))
    ;;`(font-lock-constant-face ((t (:weight bold))))
    `(font-lock-constant-face ((t (:weight normal :slant italic))))
    `(font-lock-function-name-face ((t (:weight normal))))
    ;;`(font-lock-keyword-face ((t (:weight bold :slant italic))))
    `(font-lock-type-face ((t (:slant italic)))) ;:weight bold))))
 
-   `(highlight ((t (:box (:line-width 1 :color ,(face-foreground 'default )))))) ;; font-lock-comment-face
+   ;; `(highlight ((t (:box (:line-width 1 :color ,(face-foreground 'default )))))) ;; font-lock-comment-face
 
 
    ;; `(hl-line ((t (:inherit 'nil
@@ -80,8 +103,9 @@
    ;; `(hl-line-face((t (:box nil))))
 
    ;; `(objed-hl ((t (:inherit 'nil
-   ;; 		  :background ,(face-background 'highlight) :box (:line-width 1 :color ,(face-foreground 'default) ;; font-lock-comment-face
+   ;; 		  :background ,(face-background 'highlight) :box (:line-width 1 :color ,(face-foreground 'font-lock-comment-face) ;; font-lock-comment-face
    ;; 									    )))))
+
    `(mouse ((t (:foreground ,(face-foreground 'highlight) :background ,(face-background 'default)))))
 
    ;; `(whitespace-space ((t (:foreground ,(face-background 'default) ))))
@@ -106,29 +130,29 @@
    `(line-number-current-line ((t (:inherit line-number  scale: 1.0))))
 
    ;; ;;;;; linum-relative
-     ;; `(linum-relative-current-face ((,class (:foreground ,comp))))
+   ;; `(linum-relative-current-face ((,class (:foreground ,comp))))
 
 
 
-   `(mode-line ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'default) :box (:color ,(face-foreground 'link) :line-width 2) :underline nil :overline nil))))
+   `(mode-line ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'default) :box (:color ,(face-foreground 'link) :line-width 1) :underline nil :overline nil))))
    `(mode-line-inactive ((t (:background ,(face-background 'default) :foreground ,(face-foreground 'default) :box (:color ,(face-foreground 'font-lock-comment-face) :line-width 1)))))
    `(mode-line-buffer-id ((t (:box (:color ,(face-foreground 'link) :line-width 2) :foreground ,(face-foreground 'link) :background nil :bold t))))
-   ;; '(mode-line ((t (:background "#002b36"  :foreground "white" :box "dark cyan"))))
-   ;; '(mode-line-inactive ((t (:background "#002b36" :foreground "white" :box nil))))
+   ;; ;; '(mode-line ((t (:background "#002b36"  :foreground "white" :box "dark cyan"))))
+   ;; ;; '(mode-line-inactive ((t (:background "#002b36" :foreground "white" :box nil))))
 
-    ;; `(trailing-whitespace ((t nil)))
-    `(whitespace-empty ((t nil)))
-    `(whitespace-line ((t nil)))
-    `(whitespace-newline ((t (:inherit nil :foreground ,(face-background 'default) :background ,(face-background 'default)))))
-    `(whitespace-newline-mark ((t (:inherit nil :foreground ,(face-background 'default) :background ,(face-background 'default)))))
-    `(whitespace-space ((t (:inherit nil :foreground ,(face-background 'default) :background ,(face-background 'default)))))
-    `(whitespace-spaces ((t (:inherit nil :foreground ,(face-background 'default) :background ,(face-background 'default)))))
-    `(whitespace-space-mark ((t (:inherit nil :foreground ,(face-background 'default) :background ,(face-background 'default)))))
-    `(whitespace-tabs ((t (:inherit nil :foreground ,(face-background 'default) :background ,(face-background 'default)))))
-    `(whitespace-tab ((t (:inherit nil :foreground ,(face-background 'default) :background ,(face-background 'default)))))
-    `(whitespace-trailing ((t nil)))
-    ;; `(whitespace-space-after-tab ((t nil)))
-    ;; `(whitespace-space-before-tab ((t nil)))
+   ;; `(trailing-whitespace ((t nil)))
+   `(whitespace-empty ((t nil)))
+   `(whitespace-line ((t nil)))
+   `(whitespace-newline ((t (:inherit nil :foreground ,(face-background 'default) :background nil))))
+   `(whitespace-newline-mark ((t (:inherit nil :foreground ,(face-background 'default) :background nil))))
+   `(whitespace-space ((t (:inherit nil :foreground ,(face-background 'default) :background nil))))
+   `(whitespace-spaces ((t (:inherit nil :foreground ,(face-background 'default) :background nil))))
+   `(whitespace-space-mark ((t (:inherit nil :foreground ,(face-background 'default) :background nil))))
+   `(whitespace-tabs ((t (:inherit nil :foreground ,(face-background 'default) :background nil))))
+   `(whitespace-tab ((t (:inherit nil :foreground ,(face-background 'default) :background nil))))
+   `(whitespace-trailing ((t nil)))
+   ;; `(whitespace-space-after-tab ((t nil)))
+   ;; `(whitespace-space-before-tab ((t nil)))
 
 
 
@@ -141,28 +165,37 @@
    ;;   `(whitespace-space-after-tab ((t (:background nil))))
    ;;   `(whitespace-space-before-tab ((t (:background nil))))
    ;;   `(whitespace-tab ((t (:background nil))))
-     ;; `(whitespace-trailing ((t (:background ,err ))))
+   ;; `(whitespace-trailing ((t (:background ,err ))))
 
-     ;; ;;;;; ivy
-     ;;  `(ivy-current-match ((t (;; :underline t
-     ;; 			      :inherit ivy-current-match :background ,(face-foreground 'link) ;; :foreground ,(face-background 'link)
-     ;; 			      :box (:line-width 1 :color ,(face-foreground 'default;; font-lock-comment-face
-     ;; 									   ))))))
-     ;; `(ivy-minibuffer-match-face-1 ((t (:inherit bold))))
-     ;; `(ivy-minibuffer-match-face-2 ((t (:underline t))))
-     ;; `(ivy-minibuffer-match-face-3 ((t (:underline t))))
-     ;; `(ivy-minibuffer-match-face-4 ((t (:underline t))))
+   ;; ;;;;; ivy
+   ;;  `(ivy-current-match ((t (;; :underline t
+   ;; 			      :inherit ivy-current-match :background ,(face-foreground 'link) ;; :foreground ,(face-background 'link)
+   ;; 			      :box (:line-width 1 :color ,(face-foreground 'default;; font-lock-comment-face
+   ;; 									   ))))))
+   ;; `(ivy-minibuffer-match-face-1 ((t (:inherit bold))))
+   ;; `(ivy-minibuffer-match-face-2 ((t (:underline t))))
+   ;; `(ivy-minibuffer-match-face-3 ((t (:underline t))))
+   ;; `(ivy-minibuffer-match-face-4 ((t (:underline t))))
 
-     ;; ;;;;; swiper
-     ;; `(swiper-line-face ((t (;; :underline t
-     ;; 			     :inherit swiper-line-face :background ,(face-foreground 'link) ;; :foreground ,(face-background 'link)
-     ;; 			     :box (:line-width 1 :color ,(face-foreground 'default;; font-lock-comment-face
-     ;; 									   ))))))
-     ;; `(swiper-match-face-1 ((t (:inherit bold))))
-     ;; `(swiper-match-face-2 ((t (:underline t))))
-     ;; `(swiper-match-face-3 ((t (:underline t))))
-     ;; `(swiper-match-face-4 ((t (:underline t))))
+   ;; ;;;;; swiper
+   ;; `(swiper-line-face ((t (;; :underline t
+   ;; 			     :inherit swiper-line-face :background ,(face-foreground 'link) ;; :foreground ,(face-background 'link)
+   ;; 			     :box (:line-width 1 :color ,(face-foreground 'default;; font-lock-comment-face
+   ;; 									   ))))))
+   ;; `(swiper-match-face-1 ((t (:inherit bold))))
+   ;; `(swiper-match-face-2 ((t (:underline t))))
+   ;; `(swiper-match-face-3 ((t (:underline t))))
+   ;; `(swiper-match-face-4 ((t (:underline t))))
 
+
+   ;; ;;;;; dired-subtree
+
+   `(dired-subtree-depth-1-face ((t (:background nil)))) ;; :foreground ,(face-foreground 'default)))))
+   `(dired-subtree-depth-2-face ((t (:background nil)))) ;; :foreground ,(face-foreground 'default)))))
+   `(dired-subtree-depth-3-face ((t (:background nil)))) ;; :foreground ,(face-foreground 'default)))))
+   `(dired-subtree-depth-4-face ((t (:background nil)))) ;; :foreground ,(face-foreground 'default)))))
+   `(dired-subtree-depth-5-face ((t (:background nil)))) ;; :foreground ,(face-foreground 'default)))))
+   `(dired-subtree-depth-6-face ((t (:background nil)))) ;; :foreground ,(face-foreground 'default)))))
 
    ))
 
