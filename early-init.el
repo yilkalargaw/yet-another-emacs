91fe9e3685d57ceac5acfa5afaf94c0a33aaf787
yilkalargaw <yilkalargawworkneh@gmail.com>
2024-11-15 18:13:37 +0300

    moved the font setting to early-init and trying out a new way of setting it

--- early-init.el
+++ early-init.el
@@ -48,6 +48,44 @@
                   (menu-bar-mode -1)
                   (tool-bar-mode -1)
                   ))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; some minor ui and font settings
+(set-window-margins nil 2)
+(setq inhibit-startup-message t)
+;; (global-set-key (kbd "C-c m b") 'menu-bar-mode)
+(column-number-mode 1)
+(setq initial-scratch-message nil)
+(setq battery-mode-line-format " [%b%p%% } ")
+(display-battery-mode t)
+;; (display-time-mode t)
+(setq frame-resize-pixelwise t)
+(setq redisplay-skip-fontification-on-input t)
+(setq idle-update-delay 1.0)
+(setq compilation-scroll-output t)
+
+;; (set-face-attribute 'default nil :font "Droid Sans Mono")
+;; (set-fontset-font t 'emoji (font-spec :family "Noto Color Emoji") nil 'prepend)
+
+(defun yae-set-font (&optional frame)
+  (when frame (select-frame frame))
+      (set-frame-font "JetBrainsMono Nerd Font"))
+(yae-set-font)
+(set-face-attribute 'default nil :height 105)
+(add-hook 'after-make-frame-functions 'yae-set-font)
+
+
+;; (setq default-frame-alist '((font . "JetBrainsMono Nerd Font 9"))) ;;; set default font for emacs --daemon / emacsclient
+(add-to-list 'default-frame-alist (cons 'font  my-default-font))
+(set-fontset-font t nil "Dejavu Sans Mono" nil 'append)
+(set-fontset-font t 'ethiopic "Noto Sans Ethiopic")
+;; (set-fontset-font t 'thai "Leelawadee UI" nil 'append)
+
+;; (setq w32-enable-synthesized-fonts t) ;;The w32-enable-synthesized-fonts variable is obsolete starting from Emacs 24.4, as Emacs no longer has this limitation.
+;; (set-face-font 'italic "-*-Courier New-normal-i-*-*-11-*-*-*-c-*-iso8859-1")
+;; (set-face-font 'bold-italic "-*-Courier New-bold-i-*-*-11-*-*-*-c-*-iso8859-1")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 ;; ;; increase garbage collection threshold for startup only
 ;; ;; (setq gc-cons-threshold 8000000)
